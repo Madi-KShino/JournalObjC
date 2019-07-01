40a1142fdfda15445aacace82e7867a0766dc766
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-07-01 13:55:36 -0600

    Initail Commit

---
+++ Journal(ObjC)/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal(ObjC)
+//
+//  Created by Madison Kaori Shino on 7/1/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
